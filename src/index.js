e1b3346a2f2a89bc366f51bf4f778e75c2d9c10c
arefinncode <sultanul.arefin@nordecode.com>
2018-03-15 18:06:01 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -160,3 +160,58 @@ ReactDOM.render(
     document.getElementById('body6')
 );
 
+
+
+
+
+
+ // state  last code >> https://reactjs.org/docs/state-and-lifecycle.html 
+function FormattedDate(props) {
+    return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
+class Clock3 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <FormattedDate date={this.state.date} />
+            </div>
+        );
+    }
+}
+
+function App2() {
+    return (
+        <div>
+            <Clock3 />
+            <Clock3 />
+            <Clock3 />
+        </div>
+    );
+}
+
+ReactDOM.render(<App2 />, document.getElementById('body7'));
+
